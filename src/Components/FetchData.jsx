5931a4c52e139a9f3001b3becc17f7d032decc85
mehrdadhz-learning <mehrdadhz.learning@gmail.com>
2025-09-15 14:52:57 +0200

    Completed project

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import UseFetch from './UseFetch';
+import './FetchData.css'
 
 const FetchData = () => {
+
+  // fetch the data from the api using the custom fetch function
+  const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+
+  // show the data to the output terminal as well 
+  console.log(data);
+
   return (
     <>
-     <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-     </ul>
+      <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+      <ul className='list_data_main'>
+
+        {/* go over the list of values retrieved and show the results in the front end*/}
+        {data && data.map((data_value, index) => (
+
+          <>
+            {/* for each of the elements in the list, we want to show the list of information about that item*/}
+            <li key={index} className='list_data'>
+
+              <h3>{data_value.name}</h3>
+              <p><strong>Importance : </strong> {data_value.importance}</p>
+              <p><strong>Benefits : </strong> {data_value.benefits}</p>
+              <p><strong>Time to eat : </strong> {data_value.best_time_to_intake}</p>
+            </li>
+          </>
+
+        ))}
+      </ul>
     </>
   )
 }
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
+
+// this function will call the api in order to get the data 
+// from the passed url and return the result 
 const UseFetch = (url) => {
-    
+
+    // we will consider that the data will be fetched through the API and 
+    // stored in the data variable
+    const [data, setData] = useState();
+
+    // we can use this function in order to request and process the data from the API 
+    // and change the content of the data variable
+    useEffect(() => { fetch(url).then((res) => res.json()).then((data) => setData(data)) }, [])
+
+    return [data];
 }
 
 export default UseFetch
